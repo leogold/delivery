8ccca73f3fbbbadb3edd55624802f958e3e3f7fb
leogold <leogold.br@gmail.com>
2020-07-05 17:45:43 -0300

    Debugger and Templates

--- delivery/app.py
+++ delivery/app.py
@@ -1,12 +1,14 @@
 import os
 from flask import Flask
 from delivery.ext import site
+from delivery.ext import toolbar
+from delivery.ext import config
 
 def create_app():
     """Factory principal"""
     app = Flask(__name__)
-
+    config.init_app(app)
+    toolbar.init_app(app)
     site.init_app(app)
-
     return app
 
--- delivery/ext/site/main.py
+++ delivery/ext/site/main.py
@@ -6,6 +6,19 @@ bp = Blueprint("site", __name__)
 @bp.route("/")
 def index():
     return render_template(
+        "index.html"
+    )
+
+
+@bp.route("/home")
+def home():
+    return render_template(
         "home.html", 
         name=request.args['name']
+    )
+
+@bp.route("/restaurantes")
+def restaurants():
+    return render_template(
+        "restaurants.html"
     )
